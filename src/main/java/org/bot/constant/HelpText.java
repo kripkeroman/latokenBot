a65537ac2f754cadbcb2df7f257caf3eb80a1950
r.kirilov <r.kirilov@w1.money>
2024-08-03 17:24:13 +0300

    refactoring application.properties logback.xml and HelpText

--- src/main/java/org/bot/constant/HelpText.java
+++ src/main/java/org/bot/constant/HelpText.java
@@ -7,7 +7,8 @@ public class HelpText
      */
     public static final String HELP_TEXT = "This bot was created to introduce you to the Latoken company. \n" +
             "There are several basic commands for interacting with the bot: \n\n" +
-            "Type /run - starts the bot to interact with it.\n\n" +
+            "Type /start - starts the bot to interact with it.\n\n" +
+            "Type /bot - starts the GPT from OpenAI to interact with it.\n\n" +
             "Type /help - calls this text.\n\n" +
             "Type /stop - terminates the bot.\n\n";
 }
